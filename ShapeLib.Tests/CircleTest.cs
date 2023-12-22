86d25a9cb47914027549cff285c3731dfcdaa163
Phantom <P_hanto_m@outlook.com>
2023-12-22 09:46:59 +0400

    Add tests

---
+++ ShapeLib.Tests/CircleTest.cs
@@ -0,0 +1,25 @@
+﻿using ShapeLib.Exceptions;
+using ShapeLib.Shapes;
+
+namespace ShapeLib.Tests;
+
+public class CircleTests
+{
+    [Fact]
+    public void CircleAreaCalculation()
+    {
+        var circle = new Circle<double>(5);
+        Assert.Equal(78.54, circle.GetArea(), 2);
+    }
+
+    [Fact]
+    public void CircleWithZeroRadius()
+    {
+        Assert.Throws<InvalidShapeArgumentException>(() => new Circle<double>(0));
+    }
+    [Fact]
+    public void CircleWithNegativeRadius()
+    {
+        Assert.Throws<InvalidShapeArgumentException>(() => new Circle<double>(-5));
+    }
+}
